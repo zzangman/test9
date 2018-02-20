f8e8c0ecc84c3cd5b2cb5e3b2c10d6390836a7f7
zzangman <36238128+zzangman@users.noreply.github.com>
2018-02-20 10:23:47 +0900

    Test1

--- src/com/min1/Main.java
+++ src/com/min1/Main.java
@@ -4,9 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		SchoolController sch=new SchoolController();
 		
-		sch.start();
+		System.out.println("Program Start");
+		
+		/*SchoolController sch=new SchoolController();
+		
+		sch.start();*/
 		
 	}
 
